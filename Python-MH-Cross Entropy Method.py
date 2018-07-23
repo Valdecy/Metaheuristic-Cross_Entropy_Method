845f314d3623bc97b901460a4910eb645de01e52
Valdecy Pereira <valdecypereira@yahoo.com.br>
2018-07-23 19:49:04 -0300

    Update Python-MH-Cross Entropy Method.py

--- Python-MH-Cross Entropy Method.py
+++ Python-MH-Cross Entropy Method.py
@@ -44,7 +44,6 @@ def guess_std_calc(guess):
 # Function: Generate Samples
 def generate_samples(guess, guess_mean, guess_std, min_values = [-5,-5], max_values = [5,5], k_samples = 2):
     guess_sample = guess.copy(deep = True)
-    guess_sample = guess_sample.sort_values(by = 'f(x)')
     for i in range(k_samples, guess.shape[0]):
         for j in range(0, len(min_values)):
              guess_sample.iloc[i,j] = np.clip(np.random.normal(guess_mean.iloc[0,j], guess_std.iloc[0,j], 1)[0], min_values[j], max_values[j])
@@ -53,7 +52,6 @@ def generate_samples(guess, guess_mean, guess_std, min_values = [-5,-5], max_val
 
 # Function: Update Samples
 def update_distribution(guess, guess_mean, guess_std, learning_rate = 0.7, k_samples = 2):
-    guess = guess.sort_values(by = 'f(x)')
     for j in range(0, guess_mean.shape[1]):
         guess_mean.iloc[0,j] = learning_rate*guess_mean.iloc[0,j] + (1 - learning_rate)*guess.iloc[0:k_samples,j].mean()
         guess_std.iloc[0,j] = learning_rate*guess_std.iloc[0,j] + (1 - learning_rate)*guess.iloc[0:k_samples,j].std()
@@ -70,6 +68,7 @@ def cross_entropy_method(n = 5, min_values = [-5,-5], max_values = [5,5], iterat
     count = 0
     while (count < iterations):
         print("Iteration = ", count, " f(x) = ", best[-1])
+        guess = guess.sort_values(by = 'f(x)')
         guess = generate_samples(guess, guess_mean, guess_std, min_values = min_values, max_values =  max_values, k_samples = k_samples)
         guess_mean, guess_std = update_distribution(guess, guess_mean, guess_std, learning_rate = learning_rate, k_samples = k_samples)
         if (best[-1] > guess.iloc[guess['f(x)'].idxmin(),:][-1]):
